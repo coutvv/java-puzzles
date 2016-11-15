56056d34f8f1623ddcab0ffc024f6be0d9c2588a
coutvv <coutvv@gmail.com>
2016-11-15 23:04:30 +0300

    Начал паззлы от блохав отдельные пакеты сувать
    Поменял шаблончик, ибо название в классе

--- src/main/java/ru/coutvv/puzzle/Template.java
+++ src/main/java/ru/coutvv/puzzle/Template.java
@@ -1,8 +1,6 @@
 package ru.coutvv.puzzle;
 
 /**
- * puzzle [  ]
- * 
  * why   ? 
  * fix	 ?   
  *
---
+++ src/main/java/ru/coutvv/puzzle/expressive/Oddity.java
@@ -0,0 +1,28 @@
+package ru.coutvv.puzzle.expressive;
+
+/**
+ * why   ? метод isOdd не работает для отрицательных чисел
+ * fix	 ? n%2 != 0;
+ *
+ * moral : 
+ * 			-
+ *   
+ * @author coutvv
+ */
+public class Oddity {
+
+	public static boolean isOdd(int n){
+		return n % 2 == 1;
+	}
+	
+	public static void main(String[] args) {
+		print(isOdd(2));
+		print(isOdd( 3));
+		print(isOdd(-1));
+		print(isOdd(2));
+	}
+	
+	public static void print(boolean b) {
+		System.out.println(b);
+	}
+}
