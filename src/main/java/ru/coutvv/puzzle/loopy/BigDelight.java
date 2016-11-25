bd81cb7d7bbbd0a9fac61d18b01556f7e272608a
coutvv <coutvv@gmail.com>
2016-11-25 11:35:51 +0300

    Перенёс решённый пазл в нужную категорию

--- src/main/java/ru/coutvv/puzzle/loopy/BigDelight.java
+++ src/main/java/ru/coutvv/puzzle/loopy/BigDelight.java
@@ -1,4 +1,6 @@
-package ru.coutvv.puzzle;
+package ru.coutvv.puzzle.loopy;
+
+import java.awt.geom.Point2D;
 
 /**
  * puzzle [ A big delight in every Byte ]
@@ -10,15 +12,19 @@ package ru.coutvv.puzzle;
  * 			- байты не инты!
  * 			- осторожнее с примитивами!
  * 			- сравнивайте как типизированные выражения: кастуйте или конверьте один из операндов
+ * 
+ * 			- избегайте сравнений разных типов
+ * 			- используйте константый вместо "магических чисел"
  *   
  * @author coutvv
  */
-public class ByteMe {
+public class BigDelight {
 	public static void main(String[] args) {
 		for(byte b = Byte.MIN_VALUE; b < Byte.MAX_VALUE; b++) {
 			if(b == 0x90) { // b == (byte) 0x90 // or // (b & 0xff) == 0x90
 				System.out.print("Byte me! ");
 			}
 		}
+		Point2D.Double d;
 	}
 }
